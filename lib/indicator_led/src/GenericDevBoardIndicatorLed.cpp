1eb4e1c2825c0fc4fa0ba8727904d1e7178cb631
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2025-06-08 12:28:46 +0200

    ledpin

--- lib/indicator_led/src/GenericDevBoardIndicatorLed.cpp
+++ lib/indicator_led/src/GenericDevBoardIndicatorLed.cpp
@@ -1,10 +1,9 @@
 #include "Arduino.h"
 #include "GenericDevBoardIndicatorLed.h"
 
-GenericDevBoardIndicatorLed::GenericDevBoardIndicatorLed(int ledpin)
+GenericDevBoardIndicatorLed::GenericDevBoardIndicatorLed(int ledpin): m_led_pin(led_pin)
 {
-  led_pin = ledpin;
-  pinMode(led_pin, OUTPUT);
+  pinMode(m_led_pin, OUTPUT);
 }
 
 // we don't really have any colors so just use the built in LED
@@ -12,10 +11,10 @@ void GenericDevBoardIndicatorLed::set_led_rgb(uint32_t color)
 {
   if (color == 0)
   {
-    digitalWrite(led_pin, LOW);
+    digitalWrite(m_led_pin, LOW);
   }
   else
   {
-    digitalWrite(led_pin, HIGH);
+    digitalWrite(m_led_pin, HIGH);
   }
 }
